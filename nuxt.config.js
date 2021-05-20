2d459f1ea519551440cb8591de3ee0d8b84566e6
sky0621 <sky14work@gmail.com>
2021-05-20 23:09:01 +0900

    docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,4 +67,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  router: {
+    base: '/kronologia/'
+  },
+
+  generate: {
+    dir: 'docs'
+  },
 }
